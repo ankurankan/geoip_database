f465697b57c38df15a638f3ba3093bd6cd968db0
Ankur Ankan <ankurankan@gmail.com>
2015-02-12 16:06:00 +0530

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as _test
 #print(os.getcwd())
 #response = urllib2.urlopen('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
 #file = response.read()
 #with open('geoip/GeoLite2-City.mmdb', 'wb') as f:
 #    f.write(file)
+
+class test(_test):
+    def finalize_options(self):
+        _test.finalize_option(self)
+        import geolite
+
 setup(
     name='geolite',
     description='geolite database',
