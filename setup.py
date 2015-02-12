06da422b2719bdef0d4dfb6f74942f0abe6cc2d4
Ankur Ankan <ankurankan@gmail.com>
2015-02-12 12:27:41 +0530

    setup.py for fetching GeoLite database

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import urllib2
+response = urllib2.urlopen('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
+file = response.read()
+with open('geoip/GeoLite2-City.mmdb', 'wb') as f:
+    f.write(file)
+setup(
+    name='geolite',
+    description='geolite database',
+            )
+
