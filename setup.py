cd23d2d5a7a56f02bb55728d8e2f5c2cf21d5536
Ankur Ankan <ankurankan@gmail.com>
2015-02-12 12:35:35 +0530

    debug code:

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 import urllib2
+import os
+print(os.getcwd())
 response = urllib2.urlopen('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
 file = response.read()
 with open('geoip/GeoLite2-City.mmdb', 'wb') as f:
