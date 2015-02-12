6c65507b2daf1879337fa12dfd3f0d62d1e3101a
Ankur Ankan <ankurankan@gmail.com>
2015-02-12 15:49:41 +0530

    update

--- geolite/__init__.py
+++ geolite/__init__.py
@@ -3,5 +3,5 @@ path = os.getcwd()
 
 response = urllib2.urlopen('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
 file = response.read()
-with open(os.path.join(path, 'geoip/GeoLite2-City.mmdb', 'wb') as f:
+with open(os.path.join(path, 'geoip/GeoLite2-City.mmdb'), 'wb') as f:
     f.write(file)
