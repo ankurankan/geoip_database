8ee73f6f8822a2b1f61716a4770782a653f18367
Ankur Ankan <ankurankan@gmail.com>
2015-02-12 13:32:26 +0530

    adds database

--- database/fetch.py
+++
@@ -1,8 +0,0 @@
-print("YEAH")
-import os
-import urllib2
-print(os.getcwd())
-response = urllib2.urlopen('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
-file = response.read()
-with open('GeoLite2-City.mmdb', 'wb') as f:
-    f.write(file)
